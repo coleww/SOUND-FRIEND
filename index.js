d9541f7b1dd428413c2ec51b050d4a17daa45652
Cole Willsea <coleww@gmail.com>
2016-05-24 08:47:57 -0700

    some recorder mess ugh

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // require('./songs/paths')
+
+var Recorder = require('./recorder')
+var worker = new Worker('./recorderWorkerMP3.js')
+
+
+this.recorder = new Recorder(MASTERVOLUMENODE, {}, worker)
+
+this.recorder.record()
+
+
+
+this.recorder.stop()
+this.recorder.exportAudio(function (b) {
+  console.log("GOT A WAV")
+
+  Recorder.forceDownload(b, "BLZRS--3_Things--" + fileName + '.mp3')
+})
