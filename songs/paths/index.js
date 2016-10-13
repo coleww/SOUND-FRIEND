3981720aafaebc1efee935df03ac04912950ac41
Cole Willsea <coleww@gmail.com>
2016-10-12 21:34:39 -0700

    log out guitar tab for current song

--- songs/paths/index.js
+++ songs/paths/index.js
@@ -1,43 +1,32 @@
 var config = require('./config')
 var sb = require('spiderbite')
-
-
-module.exports = function (instruments) {
+var midi = require('../../midi')
+var gtg = require('guitar-tab-generator')
+module.exports = function () {
   var seq = sb(config)
-  seq.bind(false, function (data) {
+  seq.bind(true, function (data) {
     midi.playDrum(0)
   }, require('./data/kick'))
 
-  seq.bind(false, function (data) {
-    midi.playDrum(1)
-  }, require('./data/hat'))
+  // seq.bind(false, function (data) {
+  //   midi.playDrum(1)
+  // }, require('./data/hat'))
 
-  seq.bind(false, function (data) {
-    midi.playDrum(2)
-  }, require('./data/snare'))
+  // seq.bind(false, function (data) {
+  //   midi.playDrum(2)
+  // }, require('./data/snare'))
 
   // seq.bind(false, function (data, section) {
   //   midi.playPiano(data, config.key)
   // }, require('./data/piano'))
 
-  seq.bind(true, function (data, section) {
-    midi.playSynth(data, 500, config.key)
-  }, require('./data/bass'))
+  // seq.bind(true, function (data, section) {
+  //   midi.playSynth(data, 500, config.key)
+  // }, require('./data/bass'))
 
   seq.setStructure([[1,0,0], [0,1,1]])
-
-  // might have to do the voice updating here? hrmm, if you always generate the node for the next line ahead of time....
-  // heck, just always call the first bar an "intro"///
-  seq.onSectionStart = function (update) {
-    console.log('whoo switchy!')
-    if (update) {
-      // the global current pattern thing is gonna change on the next section start yo!
-      // we can figure out what that pattern will be thanks to the seq thing. object bro. buddy!
-    } else {
-      // just grooving.
-    }
-    // instruments.voice.vocode(instruments.mainVolume)
-  }
+  var tonic = config.key.tonic.match(/\D/)[0]
+  console.log(gtg(tonic, config.key.scale))
 
   return seq
 }
