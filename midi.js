7f48857ed31fcbfd78838d56016ce6007cfed9bb
Cole Willsea <coleww@gmail.com>
2016-11-26 19:00:53 -0800

    erm

--- midi.js
+++ midi.js
@@ -2,10 +2,17 @@ var midi = require('midi')
 var help = require('midi-help')
 var freq2midi = require('midiutils').frequencyToNoteNumber
 var int2freq = require('int2freq')
-var output = new midi.output()
-console.log(output.getPortCount())
-console.log(output.getPortName(0))
-output.openPort(0)
+// var output = new midi.output()
+// console.log(output.getPortCount())
+// console.log(output.getPortName(0))
+// output.openPort(0)
+
+// // // for debugging when away from the MIDIs
+output = {
+  sendMessage: function (stuff) {
+    console.log(stuff)
+  }
+}
 
 module.exports = {
   playSynth: function (data, length, key) {
@@ -20,9 +27,9 @@ module.exports = {
   },
   playPiano: function (data, length, key) {
     var note = freq2midi(int2freq(data, key))
-    output.sendMessage(help.noteOn(note, 127))
+    output.sendMessage(help.noteOn(note, 127, 2))
     setTimeout(function () {
-      output.sendMessage(help.noteOff(note, 127, ??????))
+      output.sendMessage(help.noteOff(note, 127, 2))
     }, length)
   },
 }
--- songs/paths/index.js
+++ songs/paths/index.js
@@ -4,25 +4,25 @@ var midi = require('../../midi')
 var gtg = require('guitar-tab-generator')
 module.exports = function () {
   var seq = sb(config)
-  seq.bind(true, function (data) {
-    midi.playDrum(0)
+  seq.bind(false, function (data) {
+    midi.playDrum(data)
   }, require('./data/kick'))
 
-  // seq.bind(false, function (data) {
-  //   midi.playDrum(1)
-  // }, require('./data/hat'))
+  seq.bind(false, function (data) {
+    midi.playDrum(data)
+  }, require('./data/hat'))
 
-  // seq.bind(false, function (data) {
-  //   midi.playDrum(2)
-  // }, require('./data/snare'))
+  seq.bind(false, function (data) {
+    midi.playDrum(data)
+  }, require('./data/snare'))
 
   // seq.bind(false, function (data, section) {
   //   midi.playPiano(data, config.key)
   // }, require('./data/piano'))
 
-  // seq.bind(true, function (data, section) {
-  //   midi.playSynth(data, 500, config.key)
-  // }, require('./data/bass'))
+  seq.bind(true, function (data, section) {
+    midi.playSynth(data, 250, config.key)
+  }, require('./data/bass'))
 
   seq.setStructure([[1,0,0], [0,1,1]])
   var tonic = config.key.tonic.match(/\D/)[0]
